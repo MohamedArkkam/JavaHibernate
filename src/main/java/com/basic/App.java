cd2dde3e054a442fec31dc9da38899be37269470
Mohamed Arkkam <arkkam2000@gmail.com>
2025-07-31 17:48:29 +0530

    first commit

---
+++ src/main/java/com/basic/App.java
@@ -0,0 +1,16 @@
+package com.basic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Alien alien = new Alien();
+        alien.setAid(101);
+        alien.setAname("Zorg");
+        alien.setColor("Green");
+    }
+}
